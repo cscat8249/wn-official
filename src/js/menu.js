cb3e80d6d139e797c4415f03ea1d43b4a7b93ff4
cscat <cscat8249@naver.com>
2022-02-18 10:26:49 +0900

    메뉴 부분 수정

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,6 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
     let dep1_items = document.querySelectorAll('.dep1_item');
     let menu_items = document.querySelectorAll('.menu_items');
 
+    let all = document.querySelector('#all');
+    let meat = document.querySelector('#meat');
+    let chicken = document.querySelector('#chicken');
+    let koreafood = document.querySelector('#koreafood');
+    let globalfood = document.querySelector('#globalfood');
+    let snackbar = document.querySelector('#snackbar');
+    let dessert = document.querySelector('#dessert');
+
     dep1_items.forEach(dep1_item => {
         dep1_item.addEventListener('click', function(){
             let viewIdx = this.getAttribute('subSeq');
@@ -22,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-    var swiper = new Swiper('.all', {
+    new Swiper('.all', {
         slidesPerView: 4,
         spaceBetween: 20,
         loop : true,
@@ -35,81 +43,93 @@ document.addEventListener("DOMContentLoaded", function () {
         },
     });
 
-    var swiper = new Swiper('.meat', {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        loop : true,
-        navigation: {
-            nextEl: '.menu-swiper-left',
-            prevEl: '.menu-swiper-right',
-        },
-        mousewheel: {
-            invert: true,
-        },
+    meat.addEventListener('click', function(){
+        new Swiper('.meat', {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            loop : true,
+            navigation: {
+                nextEl: '.menu-swiper-left',
+                prevEl: '.menu-swiper-right',
+            },
+            mousewheel: {
+                invert: true,
+            },
+        });
     });
 
-    var swiper = new Swiper('.chicken', {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        loop : true,
-        navigation: {
-            nextEl: '.menu-swiper-left',
-            prevEl: '.menu-swiper-right',
-        },
-        mousewheel: {
-            invert: true,
-        },
+    chicken.addEventListener('click', function(){
+        new Swiper('.chicken', {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            loop : true,
+            navigation: {
+                nextEl: '.menu-swiper-left',
+                prevEl: '.menu-swiper-right',
+            },
+            mousewheel: {
+                invert: true,
+            },
+        });
     });
 
-    var swiper = new Swiper('.koreafood', {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        loop : true,
-        navigation: {
-            nextEl: '.menu-swiper-left',
-            prevEl: '.menu-swiper-right',
-        },
-        mousewheel: {
-            invert: true,
-        },
+    koreafood.addEventListener('click', function(){
+        new Swiper('.koreafood', {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            loop : true,
+            navigation: {
+                nextEl: '.menu-swiper-left',
+                prevEl: '.menu-swiper-right',
+            },
+            mousewheel: {
+                invert: true,
+            },
+        });
     });
 
-    var swiper = new Swiper('.globalfood', {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        loop : true,
-        navigation: {
-            nextEl: '.menu-swiper-left',
-            prevEl: '.menu-swiper-right',
-        },
-        mousewheel: {
-            invert: true,
-        },
+    globalfood.addEventListener('click', function(){
+        new Swiper('.globalfood', {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            loop : true,
+            navigation: {
+                nextEl: '.menu-swiper-left',
+                prevEl: '.menu-swiper-right',
+            },
+            mousewheel: {
+                invert: true,
+            },
+        });
     });
 
-    var swiper = new Swiper('.snackbar', {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        loop : true,
-        navigation: {
-            nextEl: '.menu-swiper-left',
-            prevEl: '.menu-swiper-right',
-        },
-        mousewheel: {
-            invert: true,
-        },
+    snackbar.addEventListener('click', function(){
+        new Swiper('.snackbar', {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            loop : true,
+            navigation: {
+                nextEl: '.menu-swiper-left',
+                prevEl: '.menu-swiper-right',
+            },
+            mousewheel: {
+                invert: true,
+            },
+        });
     });
 
-    var swiper = new Swiper('.dessert', {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        loop : true,
-        navigation: {
-            nextEl: '.menu-swiper-left',
-            prevEl: '.menu-swiper-right',
-        },
-        mousewheel: {
-            invert: true,
-        },
+    dessert.addEventListener('click', function(){
+        new Swiper('.dessert', {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            loop : true,
+            navigation: {
+                nextEl: '.menu-swiper-left',
+                prevEl: '.menu-swiper-right',
+            },
+            mousewheel: {
+                invert: true,
+            },
+        });
     });
 });
