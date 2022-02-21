d166d1572fba59a301b7825e0ab281053153cf18
cscat <cscat8249@naver.com>
2022-02-21 16:37:23 +0900

    스마트 스토어 수정

---
+++ src/js/smartstor.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var viewer = new Viewer(document.querySelector('.market_img_wrap'), {
+        navbar : false,
+        toolbar : false
+    });
+    var viewer = new Viewer(document.querySelector('.smart_img_wrap'), {
+        navbar : false,
+        toolbar : false
+    });
+});
