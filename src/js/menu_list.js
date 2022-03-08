fd0662a43a52e8db94fa840bd38282e8b790947b
parksoomin <soomin8248@gmail.com>
2022-03-08 16:30:03 +0900

    메뉴 상세페이지 layout 1차 완성

--- src/js/menu_list.js
+++ src/js/menu_list.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     let dep1_items = document.querySelectorAll('.dep1_item');
-    
+
     function menuitemListajax(mod) {
         var mod = {
             'mod' : mod
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
             var htmlArr=[]; 
             var html = '';
             for(var i=0; i < length; i++){
-                html += '<li class="item">';
+                html += '<li class="item" data-set='+data.id[i]+'>';
                 html += '<div class="item_img_wrap">';
                 html += '<img src="/wn-official/src/img/menu/'+ data.path[i] +'"alt="'+ data.menu_name[i] + '">';
                 html += '</div>';
@@ -27,16 +27,25 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             htmlArr.push(html);
             $('#menu_content').html(htmlArr);
+            let menu_items = document.querySelectorAll('li.item');
+            menu_items.forEach(item => {
+                item.addEventListener('click',function(){
+                    $("#item_reference").stop().fadeIn(300);
+                });
+            });
+            $(".pop_close").click(function(){
+                $(".item_reference").stop().fadeOut(300);
+            });
             closeLoadingWithMask();
         });
     }
     menuitemListajax();
+    
 
     dep1_items.forEach(dep1_item => {
         dep1_item.addEventListener('click', function(){
             let viewIdx = this.getAttribute('subSeq');
             let mod = this.getAttribute('id');
-            // alert(mod);
             LoadingWithMask();
             menuitemListajax(mod);
             dep1_items.forEach((thsIdx) => {
@@ -49,4 +58,5 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
+    
 });
