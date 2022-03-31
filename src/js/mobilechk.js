808bc75928409e8a4756524514bdc97ecb9832bd
parksoomin <soomin8248@gmail.com>
2022-03-31 18:51:32 +0900

    모바일 페이지이동

---
+++ src/js/mobilechk.js
@@ -0,0 +1,7 @@
+var mobileKeyWords = new Array('iPhone', 'iPod', 'BlackBerry', 'Android', 'Windows CE', 'LG', 'MOT', 'SAMSUNG', 'SonyEricsson');
+    for (var word in mobileKeyWords){
+    if (navigator.userAgent.match(mobileKeyWords[word]) != null){
+    parent.window.location.href='https://cscat8249.github.io/wn-official-mobile/';
+    break;
+    }
+}
